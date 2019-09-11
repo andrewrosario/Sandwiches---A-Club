46b68eae02e1e8627fc487212c8fd233da78990f
Anais Ahmed <anaismahmed@uchicago.edu>
2019-09-11 10:41:56 -0500

    Added home pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
     def ingredient_params
         params.require(:ingredient).permit(:name, :type_of, :img_url,
         :is_meat, :is_dairy_or_egg, :is_gluten_free)
-      end
+    end
+
+    def current_user 
+      (User.find_by(session[:user_id]) || User.new)
+    end
 end
