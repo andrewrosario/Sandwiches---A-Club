529c1f32d50cc9e36d6fdb5265ee10b281045675
andrewrosario <rosario.andrew@gmail.com>
2019-09-14 15:59:55 -0500

    aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.read_encrypted_secrets = true
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
