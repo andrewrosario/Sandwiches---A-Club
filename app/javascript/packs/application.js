553399844037f81b4434faf247732bdb6adc556b
andrewrosario <rosario.andrew@gmail.com>
2019-09-15 14:30:56 -0500

    aws

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 // require('jquery')
-require('popper')
+// require('popper')
 require('bootstrap-sprockets')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
